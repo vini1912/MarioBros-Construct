ea623c51b608556cfdaa26cec4c997a433c94f92
Vinicius oliveira <159170723+vini1912@users.noreply.github.com>
2024-06-06 08:32:26 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1717673448,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/hero-sheet0.png",
+		"images/ground_sprite_01-sheet0.png",
+		"images/ground_sprite_01-sheet1.png",
+		"images/ground_tb_01.png",
+		"images/ground_tb_02.png",
+		"images/ground_tb_03.png",
+		"images/ground_tb_04.png",
+		"images/ground_tb_05.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
